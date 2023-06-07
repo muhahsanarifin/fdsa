123942772aa679446238f82c79b75efddb72e6be
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-07 13:18:48 +0700

    Add page

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <section className="m-h-screen">
+      <Header />
+      <Layout />
+      <Footer />
+    </section>
+  );
+};
+
+export default Home;
