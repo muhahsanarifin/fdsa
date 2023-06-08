cd103a04624e8b43010c1e332028dd86dfcfd184
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-08 07:01:06 +0700

    Update component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,16 @@ const Header = () => {
   const { logo, links } = content.header;
 
   return (
-    <div className="navbar bg-[#ffffff] rounded-[16px]">
+    <div className="navbar">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">{logo}</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <details className="font-[600]">
+            <details className="font-[500]">
               <summary>Link</summary>
-              <ul className="p-2 bg-base-100">
+              <ul className="p-2 bg-base-100 z-50">
                 {links.easy_peasy.map((link, idx) => (
                   <li key={idx}>
                     <a href={link.url} target="_blank">
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import * as content from "../utils/contents";
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const { title, description } = content.layout;
+
   return (
     <div className="hero h-[30.375rem] my-8">
       <div className="hero-content flex-col lg:flex-row">
         <img
-          src="/images/stock/photo-1635805737707-575885ab0820.jpg"
+          src="src/assets/logo-easy-peasy.png"
           className="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+          <h1 className="text-5xl font-bold">{title}</h1>
+          <p className="py-6">{description}</p>
+          <button className="btn glass" onClick={() => navigate("/example")}>
+            Example
+          </button>
         </div>
       </div>
     </div>
