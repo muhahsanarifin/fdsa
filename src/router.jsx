a83ffef3cdf7b073a3db0adee17fb33222197685
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-08 07:00:50 +0700

    Update router

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home  from "./pages/home";
+import Home from "./pages/home";
+import Example from "./pages/example";
+import Title from "./components/title";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <Title onTitle={"Home"}>
+        <Home />
+      </Title>
+    ),
+  },
+  {
+    path: "example",
+    element: (
+      <Title onTitle={"Example"}>
+        <Example />
+      </Title>
+    ),
   },
 ]);
 
