1dd200899335a7e0c8d673ec10e3d6237d6b1a1c
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-19 10:49:34 +0700

    Update example.js

--- src/pages/example.jsx
+++ src/pages/example.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as Layout from "../components/Layout";
 import * as Alert from "../components/Alert";
 import { Breadcrumbs } from "../components/Breadcrumbs";
+import * as storage from "../utils/storage/local";
 
 const Example = () => {
+  const [hideAlert, setHideAlert] = useState(
+    storage.get("hideAlert") ? true : false
+  );
+
+  const handleCloseAlert = () => {
+    setHideAlert(true);
+    storage.set("hideAlert", true);
+  };
+
   return (
-    <section className="min-h-screen flex flex-col w-screen">
+    <section className="min-h-screen flex flex-col">
       {/* Info section */}
-      {false && <Alert.Info />}
+      {!hideAlert && <Alert.Info setHandleCloseAlert={handleCloseAlert} />}
 
       {/* Breadcrumbs section */}
       <Breadcrumbs onELements={["Home", "Example"]} />
