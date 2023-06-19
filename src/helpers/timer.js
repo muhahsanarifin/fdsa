5ccea77b075dd625b7f78b02fd8d320b4c55ea1d
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-19 10:49:17 +0700

    Update timer.js

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -3,3 +3,7 @@ import { DateTime } from "luxon";
 export const date = (date) => {
   return DateTime.fromMillis(date).toFormat("DDDD");
 };
+
+export const convertDate = (date) => {
+  return DateTime.fromMillis(date).toFormat("yyyy-MM-dd");
+}
