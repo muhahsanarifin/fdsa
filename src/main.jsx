7b0c39643e1b80dffab5f8e3e6ca0d58a3661a8f
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-06-19 15:56:16 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./index.css";
-
+import { StoreProvider } from "easy-peasy";
+import store from "./easy-peasy/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StoreProvider store={store}>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </React.StrictMode>
 );
