cfd806fe49f8942a19a49477561c25928b9eeaf1
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-07-01 15:35:26 +0700

    Update home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <section className="m-h-screen sm:w-screen sm:px-2">
+    <section className="m-h-screen w-[80vw] sm:w-screen sm:px-2">
       <Header />
       <Layout.Home />
       <Footer />
